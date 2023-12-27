5398e264cb9df2872b79ddd688f5dd5699d3be69
Gargouillis <150638831+Gargouillis@users.noreply.github.com>
2023-12-27 21:49:20 +0000

    commit

--- LaListeDeCourse.py
+++ LaListeDeCourse.py
@@ -39,6 +39,7 @@ while choix == "":
 while choix != "5":
     if choix == "3":
         print(liste_de_course)
+        print("-----------------------------------------------------------------------")
         print("1: Ajouter un élément à la liste de courses")
         print("2: Retirer un élément de la liste de courses")
         print("3: Afficher les éléments de la liste de courses")
@@ -60,6 +61,8 @@ while choix != "5":
     elif choix == "1":
         nouvel_ingredient = input("Saisir un ingrédient : ")
         liste_de_course.append(nouvel_ingredient)
+        print(f"L'ingrédient {nouvel_ingredient} a été ajouté à la liste")
+        print("-----------------------------------------------------------------------")
         print("1: Ajouter un élément à la liste de courses")
         print("2: Retirer un élément de la liste de courses")
         print("3: Afficher les éléments de la liste de courses")
@@ -81,6 +84,8 @@ while choix != "5":
     elif choix == "2":
         suppression_ingredient = input("Ingredient à supprimer : ")
         liste_de_course.remove(suppression_ingredient)
+        print(f"L'ingrédient {suppression_ingredient} a été supprimé")
+        print("-----------------------------------------------------------------------")
         print("1: Ajouter un élément à la liste de courses")
         print("2: Retirer un élément de la liste de courses")
         print("3: Afficher les éléments de la liste de courses")
@@ -101,6 +106,8 @@ while choix != "5":
                 pass
     elif choix == "4":
         liste_de_course.clear()
+        print("La liste de course a été supprimé....")
+        print("-----------------------------------------------------------------------")
         print("1: Ajouter un élément à la liste de courses")
         print("2: Retirer un élément de la liste de courses")
         print("3: Afficher les éléments de la liste de courses")
