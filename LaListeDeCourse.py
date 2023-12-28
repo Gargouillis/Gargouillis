add8f65831f145429b8719fb87300efcbe16f264
Gargouillis <150638831+Gargouillis@users.noreply.github.com>
2023-12-28 20:10:14 +0000

    commit

--- LaListeDeCourse.py
+++ LaListeDeCourse.py
@@ -13,6 +13,11 @@ Le programme doit permettre de réaliser 5 actions :
 
 """
 
+def affichage_liste(liste_de_course):
+    for i in range(len(liste_de_course)):
+        print(f"{i+1}.  {liste_de_course[i]}")
+
+
 def menu_liste_de_course():
     print("1: Ajouter un élément à la liste de courses")
     print("2: Retirer un élément de la liste de courses")
@@ -20,18 +25,16 @@ def menu_liste_de_course():
     print("4: Vider la liste de courses")
     print("5: Quitter le programme")
 
-
 def menu_choix_utilisateur(valeur_choix):
-    if choix.isdigit():
-                if int(choix) in (1, 2, 3, 4, 5):
-                    KO = False
-                else:
-                    print("Choix entre 1 et 5 uniquement")
+    if valeur_choix.isdigit():
+        if int(valeur_choix) in (1, 2, 3, 4, 5):
+            return False
+        else:
+            print("Choix entre 1 et 5 uniquement")
+            return True
     else:
         print("Choisir un chiffre numérique entre 1 et 5")
-    return
-
-
+    return True
 
 liste_de_course = []
 choix = ""
@@ -40,92 +43,56 @@ while choix == "":
     menu_liste_de_course()
     KO = True
     while KO == True:
-        try:
-            choix = input("Votre choix : ")
-            if choix.isdigit():
-                if int(choix) in (1, 2, 3, 4, 5):
-                    KO = False
-                else:
-                    print("Choix entre 1 et 5 uniquement")
-            else:
-                print("Choisir un chiffre numérique entre 1 et 5")
-        except:
-            pass
-
+        choix = input("Votre choix : ")
+        KO = menu_choix_utilisateur(choix)
 
 while choix != "5":
     if choix == "3":
-        print(liste_de_course)
-        print("-----------------------------------------------------------------------")
+        print("-" * 50)
+        print("Voici la liste de course")
+        affichage_liste(liste_de_course)
+        # print(liste_de_course)
+        print("-" * 50)
         menu_liste_de_course()
         KO = True
         while KO == True:
-            try:
-                choix = input("Votre choix : ")
-                if choix.isdigit():
-                    if int(choix) in (1, 2, 3, 4, 5):
-                        KO = False
-                    else:
-                        print("Choix entre 1 et 5 uniquement")
-                else:
-                    print("Choisir un chiffre numérique entre 1 et 5")
-            except:
-                pass
+            choix = input("Votre choix : ")
+            KO = menu_choix_utilisateur(choix)
     elif choix == "1":
+        print("-" * 50)
         nouvel_ingredient = input("Saisir un ingrédient : ")
         liste_de_course.append(nouvel_ingredient)
         print(f"L'ingrédient {nouvel_ingredient} a été ajouté à la liste")
-        print("-----------------------------------------------------------------------")
+        print("-" * 50)
         menu_liste_de_course()
         KO = True
         while KO == True:
-            try:
-                choix = input("Votre choix : ")
-                if choix.isdigit():
-                    if int(choix) in (1, 2, 3, 4, 5):
-                        KO = False
-                    else:
-                        print("Choix entre 1 et 5 uniquement")
-                else:
-                    print("Choisir un chiffre numérique entre 1 et 5")
-            except:
-                pass
+            choix = input("Votre choix : ")
+            KO = menu_choix_utilisateur(choix)
     elif choix == "2":
         suppression_ingredient = input("Ingredient à supprimer : ")
-        liste_de_course.remove(suppression_ingredient)
-        print(f"L'ingrédient {suppression_ingredient} a été supprimé")
-        print("-----------------------------------------------------------------------")
+        try:
+            print("-" * 50)
+            liste_de_course.remove(suppression_ingredient)
+            print(f"L'ingrédient {suppression_ingredient} a été supprimé")
+        except:
+            print("L'ingédient n'est pas dans la liste")
+        print("-" * 50)
         menu_liste_de_course()
         KO = True
         while KO == True:
-            try:
-                choix = input("Votre choix : ")
-                if choix.isdigit():
-                    if int(choix) in (1, 2, 3, 4, 5):
-                        KO = False
-                    else:
-                        print("Choix entre 1 et 5 uniquement")
-                else:
-                    print("Choisir un chiffre numérique entre 1 et 5")
-            except:
-                pass
+            choix = input("Votre choix : ")
+            KO = menu_choix_utilisateur(choix)
     elif choix == "4":
+        print("-" * 50)
         liste_de_course.clear()
         print("La liste de course a été supprimé....")
-        print("-----------------------------------------------------------------------")
+        print("-" * 50)
         menu_liste_de_course()
         KO = True
         while KO == True:
-            try:
-                choix = input("Votre choix : ")
-                if choix.isdigit():
-                    if int(choix) in (1, 2, 3, 4, 5):
-                        KO = False
-                    else:
-                        print("Choix entre 1 et 5 uniquement")
-                else:
-                    print("Choisir un chiffre numérique entre 1 et 5")
-            except:
-                pass
+            choix = input("Votre choix : ")
+            KO = menu_choix_utilisateur(choix)
 
 print("Programme terminé")
+
