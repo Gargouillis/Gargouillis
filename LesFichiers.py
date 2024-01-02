0f6862f55099ed5d44b26c3f4f83763581dddb84
Gargouillis <150638831+Gargouillis@users.noreply.github.com>
2024-01-02 17:52:19 +0000

    commit

---
+++ LesFichiers.py
@@ -0,0 +1,10 @@
+# ctrl + d permet de changer des meme caractère en meme temps
+# 3 facons de mettre un chemin windows
+
+# le double \ permet de ne pas que Python interprete \t comme une tabulation par exemple
+chemin = "C:\\Formation_Udemy\\Python_A_a_Z"
+# la foicntion r (= raw string) force python a bien prendre en compte \ et ne pas interpreter \t par exemple
+chemin = r"C:\Formation_Udemy\Python_A_a_Z"
+# ou alors on met le slash normal et python sait que c'est un chemin windows
+chemin = "C:/Formation_Udemy/Python_A_a_Z"
+print(chemin)
