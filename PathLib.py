47be6e8a15128fe8cff8e2683e221c0e0e5222d0
Gargouillis <150638831+Gargouillis@users.noreply.github.com>
2024-01-03 20:14:17 +0000

    commit

--- PathLib.py
+++ PathLib.py
@@ -1,9 +1,36 @@
 from pathlib import Path
-
+"""
+# on recupère le répertoire de travail
 print(Path.home())
+# on récupère le répertoire courant
 print(Path.cwd())
 
 p = Path("/users/Documents/fichier.txt")
 print(p)
 print(p.parent)
 print(p.suffix)
+
+t = Path("users")
+print(t)
+
+t = t / "Documents" / "fichier.txt"
+print(t)
+
+r = Path("users")
+print(r.joinpath("Documents", "liste_course.json"))
+print(r.joinpath("Documents", "liste_course.json").suffix)   """
+
+s = Path("/workspaces/Gargouillis/LesFichiers.py")
+
+print(s.name)
+print(s.parent.parent)
+print(s.parent)
+print(s.stem)
+print(s.suffix)
+print(s.suffixes)
+print(s.parts)
+print(s.exists())
+print(s.is_dir())
+print(s.is_file())
+
+print(dir(s))
